6714c4ed0b378bb1e2e0ed087d0da4c61d09bac3
pfjarschel <pfjarschel@gmail.com>
2022-12-06 11:06:05 -0300

    USB test OK

--- remotevisa_test.py
+++ remotevisa_test.py
@@ -6,13 +6,17 @@ remotevisa.commsMan.StartCommunications("143.106.153.67", 8080)
 rm = remotevisa.ResourceManager()
 res_list = rm.list_resources()
 print(res_list)
-dev_id = res_list[0]
-dev = rm.open_resource(dev_id)
+dev0_id = res_list[0]
+dev0 = rm.open_resource(dev0_id)
+dev1_id = res_list[1]
+dev1 = rm.open_resource(dev1_id)
 # dev = rm.open_resource("GPIB0::20::INSTR")
 
-print(dev.query("*IDN?"))
+print(dev0.query("*IDN?"))
+print(dev1.query("*IDN?"))
 
-dev.close()
+dev0.close()
+dev1.close()
 
 
 remotevisa.commsMan.CloseCommunications()
